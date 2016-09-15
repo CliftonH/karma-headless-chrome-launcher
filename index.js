58940c1d8529ea4b72298d013cd6b5bffa742b30
Clifton Hensley <chensley@atlassian.com>
2016-09-14 23:44:07 -0500

    cleaning up

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-var os = require('os');
-var path = require('path');
-var rimraf = require('rimraf');
+var q = require('q');
 var spawn = require('child_process').spawn;
 var whichSync = require('which').sync;
 var Xvfb = require('xvfb');
@@ -9,23 +7,34 @@ var chromeLocation = require('chrome-location');
 var HeadlessChromeBrowser = function(baseBrowserDecorator, args) {
 
   var self = this;
-  var chromeProcess;
   baseBrowserDecorator(this);
 
   this.name = 'HeadlessChrome';
+
+  var xvfb = new Xvfb({
+    silent: true,
+    xvfb_args: [
+      '-screen',
+      '0',
+      '1024x768x24',
+      '-ac'
+    ]
+  });
+
+  var chromeProcess;
 
   this.start = function (url) {
 
     if (!chromeLocation) {
       console.error('Cannot find Chrome executable.');
-      this.kill();
+      self.kill();
     }
 
     try {
       whichSync('Xvfb');
     } catch (e) {
       console.error('Cannot find Xvfb executable.');
-      this.kill();
+      self.kill();
     }
 
     var chromeFlags = [
@@ -38,36 +47,38 @@ var HeadlessChromeBrowser = function(baseBrowserDecorator, args) {
       '--disable-background-timer-throttling'
     ].concat(args.chromeFlags || [], [url]);
 
-    var xvfb = new Xvfb({
-      silent: true,
-      xvfb_args: [
-        '-screen',
-        '0',
-        '1024x768x24',
-        '-ac'
-      ]
-    });
-
-    xvfb.startSync();
-
-    chromeProcess = spawn(chromeLocation, chromeFlags);
-
-    chromeProcess.on('exit', function() {
-      xvfb.stopSync();
+    xvfb.start(function(err) {
+      if (err) {
+        console.error('Failed to start Xvfb: ' + err);
+        self.kill();
+        return;
+      }
+      chromeProcess = spawn(chromeLocation, chromeFlags);
     });
 
   };
 
   this.kill = function (done) {
     function allDone() {
-      chromeProcess.exit();
+      self._done();
       if (done) {
         done();
       }
-      self._done();
     }
-    process.nextTick(allDone);
+    let deferred = q.defer();
+    xvfb.stop(function() {
+      chromeProcess.kill('SIGINT');
+      deferred.promise.then(allDone);
+    });
   };
+
+  this.forceKill = function () {
+    var self = this;
+
+    return q.promise(function (resolve) {
+      self.kill(resolve);
+    });
+  }
 };
 
 module.exports = {
